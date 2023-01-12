7cd6b54fb92017d9d47c7d4bb0790264b01ce30d
umut <umut.d2.cgo@gmail.com>
2023-01-13 01:44:36 +0300

    Little improvements.

--- slotGame.py
+++ slotGame.py
@@ -1,34 +1,26 @@
 import random
-# symbols Y
-# lines Y
-# reels Y
-# bet multiplier
-# bonus round Y
-# max bet Y
-# max line min line
 
-# 6 tane sembol var ve her sembolün değeri var.
-# 3 tane dikey sıra var ve dikey sıra 3 tane sembol içeriyor.
+# 5 tane sembol var ve her sembolün değeri var.
+# 3 tane dikey ve yatay sıra var ve 3 tane sembol içeriyor.
 # Herhangi bir sembolden 3 tanesi ödeme çizgisine denk gelirse kazanılır.
 # Ödeme çizgileri kazanıldığında oynanılan miktar sembolün değeri ile çarpılır.
-# Örnek: 1 TL oynanıp 3 tane karpuz sembolü çıktıysa 2 TL kazanılır.
+# Örnek: 1 TL oynanıp 3 tane Kavun sembolü çıktıysa 5 TL kazanılır.
 
-semboller = ["cilek", "limon", "kavun", "armut","bonus"] #Sembollerin listesi.	
-semboller_deger = {"cilek": 8, "limon": 7, "armut": 6, "kavun": 5,"bonus": 10} # Sembollerin değerleri.	
+semboller = ["Çilek", "Limon", "Kavun", "Armut","Bonus"] #Sembollerin listesi. 
+semboller_deger = {"Çilek": 8, "Limon": 7, "Armut": 6, "Kavun": 5,"Bonus": 1} # Sembollerin değerleri. 
 
 kazandiran_cizgiler = [[0,1,2],[3,4,5],[6,7,8], # Yatay olarak kazandıran çizgi.
             [0,4,8],[2,4,6], # Çapraz olarak kazandıran çizgi.
             [0,3,6],[1,4,7],[2,5,8], # Dikey olarak kazandıran çizgi.
-            [0,4,2],[6,4,8]]
-max_bet = 10 # Oynanabilecek maksimum miktar.	
-min_bet = 1 # Oynanabilecek minimum miktar.	
-min_line = 3
-max_line = 5
-balance = 200
-def kac_tur(): # Kullanıcıdan kaç tur oynamak istediğini alır.	
+            [0,4,2],[6,4,8]] # Yukarıdan ve aşağıdan V şeklinde kazandıran çizgi.
+max_bet = 10 # Oynanabilecek maksimum miktar. 
+min_bet = 1 # Oynanabilecek minimum miktar.
+balance = 500
+
+def kac_tur(): # Kullanıcıdan kaç tur oynamak istediğini alır. 
     try:
         tur = int(input("Kaç tur oynamak istersiniz:"))
-    except ValueError: # Kullanıcı sayı girmeyip başka bir karakter girerse hata verir.	
+    except ValueError: # Kullanıcı sayı girmeyip başka bir karakter girerse hata verir. 
         print("Lütfen sayı giriniz.")
     return tur
 
@@ -40,7 +32,7 @@ def bahis_yap(tur): #Kullanıcıdan oynanacak miktarı alır.
                 print(f"En fazla {max_bet} TL oynayabilirsiniz.")
             elif bet < min_bet:
                 print(f"En az {min_bet} TL oynamalısınız.")
-            if balance < bet * tur : # Kullanıcının bakiyesi oynanacak miktarın tur sayısıyla çarpımının 3 katından(çizgi başına) azsa hata verir.
+            if balance < ( bet * tur * len(kazandiran_cizgiler) ) : # Kullanıcının bakiyesi oynanacak miktarın tur sayısıyla çarpımının 10 katından(çizgi başına) azsa hata verir.
                 print(f"Yeterli bakiyeniz bulunmamaktadır. En fazla {balance} TL oynayabilirsiniz.")
             elif balance == 0 or balance < 0:
                 print("Bakiyeniz bulunmamaktadır. Çıkış yapılıyor... /n Tekrar bekleriz.")
@@ -52,13 +44,13 @@ def bahis_yap(tur): #Kullanıcıdan oynanacak miktarı alır.
 
 def spin(bahis_miktari,tur): #Kullanıcıdan alınan miktar kadar oyunu oynatır.
     global balance # Kullanıcının bakiyesini global olarak tanımlar. global should always be defined inside a function, the reason for this is because it's telling the function that you wanted to use the global variable instead of local ones.
-    total_spin_payouts = 0 # Toplam kazanç miktarını tutar.
-    payouts = [] 
+    total_payout = 0 # Toplam kazanç miktarını tutar.
     total_loss = 0 # Toplam kayıp miktarını tutar.
     
     for i in range(tur):
         kazanan_cizgiler = [] # Kazanan çizgileri tutar.
-        total_payout = 0 # Her tur için kazanç miktarını tutar.
+        payout = 0 # Her tur için kazanç miktarını tutar.
+        total_loss += (bahis_miktari * len(kazandiran_cizgiler)) # Her tur için kayıp miktarının toplam kayıp miktarına eklenmesi.
         
         result1,result2,result3 = random.choices(semboller,weights=[22.5,22.5,22.5,22.5,10],k=3) # random.choices() fonksiyonu ile semboller listesinden 3 tane sembol seçer. weights parametresi ile sembolün çıkma olasılığını belirler.
         result4,result5,result6 = random.choices(semboller,weights=[22.5,22.5,22.5,22.5,10],k=3)
@@ -67,57 +59,51 @@ def spin(bahis_miktari,tur): #Kullanıcıdan alınan miktar kadar oyunu oynatır
         
         print(f"{result1}  |  {result2}  |  {result3} \n{result4}  |  {result5}  |  {result6} \n{result7}  |  {result8}  |  {result9} ") # Seçilen sembollerin ekrana yazdırılması.
         for kazanan_cizgi in kazandiran_cizgiler:
-            payout = 0 
             if results[kazanan_cizgi[0]] == results[kazanan_cizgi[1]] == results[kazanan_cizgi[2]]: # Kazanan çizgilerin bulunması.
-                payout = (semboller_deger[results[kazanan_cizgi[0]]] * bahis_miktari) # Kazanan çizgilerin değerlerinin bulunması.
-                total_loss += (bahis_miktari * 3) # Her tur için kayıp miktarının toplam kayıp miktarına eklenmesi.
-                payouts.append(payout)
-                if results[kazanan_cizgi[0]] and [kazanan_cizgi[1]] and [kazanan_cizgi[2]] == "bonus": # Bonus sembolünün çıkması durumunda bonus fonksiyonunu çağırır.
-                    print("Bonus kazandınız")
-                    total_payout += bonus() # Bonus fonksiyonundan dönen değeri kazanç miktarına ekler.
-                    total_spin_payouts += payout
-            if payout > 0:
-                total_payout += payout # Kazanç miktarının bir tur için toplam kazanç miktarına eklenmesi.
-                total_spin_payouts += payout # Kazanç miktarının bütün turlar için toplam kazanç miktarına eklenmesi.
+                payout = payout + (semboller_deger[results[kazanan_cizgi[0]]] * bahis_miktari) # Kazanan çizgilerin değerlerinin bulunması, kazanç miktarının hesaplanması ve kazanç miktarının bir tur için toplam kazanç miktarına eklenmesi.
+                total_payout += payout # Kazanç miktarının bütün turlar için toplam kazanç miktarına eklenmesi.
                 kazanan_cizgiler.append(kazanan_cizgi) # Kazanan çizgileri listeye ekler.
-        if total_payout == 0:
-            print(f"Kazanamadınız. {(bahis_miktari * 3)} TL kaybettiniz.")
-            total_loss += (bahis_miktari * 3)
+                if results[kazanan_cizgi[0]] and [kazanan_cizgi[1]] and [kazanan_cizgi[2]] == "Bonus": # Bonus sembolünün çıkması durumunda Bonus fonksiyonunu çağırır.
+                    print("Bonus kazandınız")
+                    payout += Bonus() # Bonus fonksiyonundan dönen değeri kazanç miktarına ekler.
+                    total_payout += payout # Bonus fonksiyonundan dönen değeri toplam kazanç miktarına ekler.
+        if payout == 0:
+            print(f"Kazanamadınız. {(bahis_miktari * len(kazandiran_cizgiler))} TL kaybettiniz. \n")
         else:
-            print(f"Kazandınız {total_payout} TL")
-            print(f"Şu çizgilerde kazandınız {kazanan_cizgiler}")
-    total_fark = total_spin_payouts - total_loss # Toplam kazanç miktarından toplam kayıp miktarını çıkarır.
+            print(f"Kazandınız {payout} TL")
+            print(f"Şu çizgilerde kazandınız {kazanan_cizgiler} \n")
+    total_fark = total_payout - total_loss # Toplam kazanç miktarından toplam kayıp miktarını çıkarır.
     balance = balance + total_fark # kullanıcının bakiyesini düzenler.
-    if total_spin_payouts > 0: # Toplam kazanç miktarı toplam kayıp miktarından büyükse kullanıcıya kazandığını belirtir.
-        print(f"Toplamda {total_fark} TL kazandınız.(Kazanılan miktar: {total_spin_payouts} TL - Kaybedilen miktar: {total_loss} TL)")
+    if total_payout > 0: # Toplam kazanç miktarı toplam kayıp miktarından büyükse kullanıcıya kazandığını belirtir.
+        print(f"Toplamda {total_fark} TL kazandınız.(Kazanılan miktar: {total_payout} TL - Kaybedilen miktar: {total_loss} TL)")
     else: # Toplam kazanç miktarı toplam kayıp miktarından küçükse kullanıcıya kaybettiğini belirtir.
-        print(f"Toplamda {total_fark} TL kaybettiniz.(Kazanılan miktar: {total_spin_payouts} TL - Kaybedilen miktar: {total_loss} TL)")
+        print(f"Toplamda {total_fark} TL kaybettiniz.(Kazanılan miktar: {total_payout} TL - Kaybedilen miktar: {total_loss} TL)")
 
-def bonus(): #Bonus oyunu
-    wheel = ["cilek", "limon", "kavun", "armut","boş"]
+def Bonus(): #Bonus oyunu
+    wheel = ["Çilek", "Limon", "Kavun", "Armut","boş"]
     wheel_result = random.choices(wheel,weights=[22.5,22.5,22.5,22.5,10],k=1)
     print("Çark çevriliyor...")
     print(f"Çarkta {wheel_result} çıktı.")
     if wheel_result == "boş":
         print("Kaybettiniz")
-        return 0
+        return 0 # Bonus oyunundan gelen değeri return eder.
     else:
         print(f"Kazandınız {semboller_deger[wheel_result] * 5} TL")
         return (semboller_deger[wheel_result] * 5) # Bonus oyunundan gelen değeri return eder.
 
-def main():
+def main(): 
     tur = kac_tur() # Kullanıcıdan kaç tur oynayacağı sorulur.
-    spin(bahis_yap(tur),tur) # Bahis yapılır ve spin fonksiyonu çağırılır.
+    bahis = bahis_yap(tur) # Bahis yapılır.
+    spin(bahis,tur) # Spin fonksiyonu çağırılır.
     while True:
         try:
             print(f"Bakiyeniz: {balance} TL")
-            devam = input("Tekrar oynamak ister misiniz? (E/H) Çıkış yapmak için Q tuşlayınız.: ")
-            if devam == "Q" or devam == "q":
-                print("Oyun sonlandırılıyor...")
-                quit()
-            if devam == "E" or devam == "e":
-                spin(bahis_yap(tur),tur)
-            elif devam == "H" or devam == "h":
+            devam = input("Tekrar oynamak ister misiniz? (E/H)").lower()
+            if devam == "e":
+                tur = kac_tur()
+                bahis = bahis_yap(tur)
+                spin(bahis,tur)
+            elif devam == "h":
                 print("Oyun sonlandırılıyor...")
                 break
             else:
